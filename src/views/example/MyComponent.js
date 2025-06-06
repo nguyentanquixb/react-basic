beef47c675f68bebbaafe79e9a04044cfd16ea1e
nguyentanquixb <nguyentanquixb@gmail.com>
2025-06-06 16:19:26 +0700

    update funtion as props

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import ChildComponent from "./ChildComponent";
+import AddComponent from "./AddComponent";
 
 class MyComponent extends Component {
 
   state = {
-    firstName: '',
-    lastName: '',
     arrJob:[
       {id:1, title:'dev', salary:'500'},
       {id:2, title:'tester', salary:'400'},
@@ -13,32 +12,19 @@ class MyComponent extends Component {
     ]
   } 
 
-  handleOnchangeFirstName = (event)=>{
+  addNewJob = (job)=>{
+    console.log('job',job)
     this.setState({
-      firstName: event.target.value
+      arrJob: [...this.state.arrJob,job]
     })
-  }
-  handleOnchangeLastName = (event)=>{
-    this.setState({
-      lastName: event.target.value
-    })
-  }
-  submit = (event)=>{
-    event.preventDefault();
-    console.log('check data state',this.state)
   }
 
   render() {
     return (
       <React.Fragment>
-        <form >
-          <label htmlFor="fname">First name:</label><br/>
-          <input type="text" id="fname" name="fname" value={this.state.firstName} onChange = {(event)=>{this.handleOnchangeFirstName(event)}} /><br/>
-          <label htmlFor="lname">Last name:</label><br/>
-          <input type="text" id="lname" name="lname" value={this.state.lastName} onChange = {(event)=> {this.handleOnchangeLastName(event)}} /><br/><br/>
-          <input type="submit" value="Submit" onClick ={(event)=> {this.submit(event)}} />
-        </form> 
-        <ChildComponent name ={this.state.firstName} age = {'25'} arrJob ={this.state.arrJob} />
+        <AddComponent addNewJob={this.addNewJob} />
+        <ChildComponent  arrJob ={this.state.arrJob} />
+    
       </React.Fragment>
     );
   }
